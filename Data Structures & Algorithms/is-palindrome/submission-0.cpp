fe9818686bb0bb967aeda620626303fa30717ede
sky17092004 <b23ee1103@iitj.ac.in>
2026-08-09 19:35:14 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string ans="";
+        for(char c:s){
+            if(isalnum(c)){
+                ans+=tolower(c);
+            }
+        }
+        int i=0;
+        int j=ans.size()-1;
+        while(i<j){
+            if(ans[i]!=ans[j]){
+                return false;
+            }
+            i++;
+            j--;
+
+        }
+        return true;
+        
+    }
+};
